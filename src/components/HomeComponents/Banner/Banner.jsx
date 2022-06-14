2ffc3af8929a2b16b66160996bdae7819dc4ca14
Suhag Al Amin <developersuhag@gmail.com>
2022-06-15 00:10:23 +0600

    navbar

---
+++ src/components/HomeComponents/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Navigation from "../../Navigation/Navigation";
+import "./Banner.css";
+import quote from "../../../images/quote.svg";
+import din from "../../../images/din-roda.png";
+
+const Banner = () => {
+  return (
+    <div className="banner-section">
+      <Navigation />
+      <Container fluid className="pt-5">
+        <Row className="">
+          <Col md={6} className=" d-none d-md-block">
+            <div className=" banner-col text-end">
+              <div className="text-end">
+                <img src={quote} alt="" />
+              </div>
+              <h2 className="my-4">
+                effective business conduct based on up-to-date technology is the
+                basis for success in the current era.
+              </h2>
+              <h3 className="my-2">Din Roda</h3>
+              <p>I am a Full Stack Developer</p>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div className="din-roda text-end">
+              <img src={din} alt="" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
