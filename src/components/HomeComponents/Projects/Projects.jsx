a91add1463cc1129d0f9a8b803a83cb8255dd5e6
Suhag Al Amin <developersuhag@gmail.com>
2022-06-15 20:11:51 +0600

    prject des

--- src/components/HomeComponents/Projects/Projects.jsx
+++ src/components/HomeComponents/Projects/Projects.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { Card, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Accordion, Button, Card, Container, Fade } from "react-bootstrap";
 import "./Projects.css";
 import projectThumb from "../../../images/project-thumb.svg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { FiLink2 } from "react-icons/fi";
+import { BsChevronDown } from "react-icons/bs";
+import checkCircle from "../../../images/check-circle.svg";
 
 // slider next prev
 const PreviousBtn = (props) => {
@@ -182,6 +186,16 @@ const Projects = () => {
       },
     ],
   };
+
+  // handle onclick
+  const [isClicked, setIsClicked] = useState(false);
+  const [clickedId, setClickedId] = useState(0);
+
+  const handleOnclick = (id) => {
+    setIsClicked("clicked");
+    setClickedId(id);
+  };
+
   return (
     <div className="projects-section">
       <Container className="py-5">
@@ -190,19 +204,80 @@ const Projects = () => {
           <Slider {...settings}>
             {projects.map((project) => (
               <div key={project.id} className="single-slide">
-                <Card className="p-4 h-100">
+                <Card className="pt-4 ps-4 pe-4">
                   <Card.Img variant="top" src={project.thumb} />
-                  {console.log(project)}
                   <Card.Body>
                     <Card.Title className="project-title">
                       {project.title}
                     </Card.Title>
-                    <Card.Text className="tech-skills-des">
-                      The purpose of the meeting is to give the business owner a
-                      centralized and organized report with the findings and
-                      recommendations for turning the business into an
-                      automated, efficient and profitable business.
-                    </Card.Text>
+                    <div className="d-flex justify-content-end gap-4">
+                      <div>
+                        <AiOutlineHeart />
+                        <span>{project.like}</span>
+                      </div>
+                      <div>
+                        <a
+                          href="http://example.com"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <FiLink2 />
+                        </a>
+                      </div>
+                      <div>
+                        <Button onClick={() => handleOnclick(project.id)}>
+                          View Details <BsChevronDown />{" "}
+                        </Button>
+                      </div>
+                    </div>
+                    <div
+                      className={`${"project-details-box"} ${
+                        isClicked === "clicked" && clickedId === project.id
+                          ? "clicked-box"
+                          : ""
+                      }`}
+                    >
+                      {isClicked === "clicked" && clickedId === project.id ? (
+                        <div>
+                          <div className="project-des">
+                            <h3 className="project-box-title">Description</h3>
+                            <p className="project-des">{project.description}</p>
+                          </div>
+                          <div className="features">
+                            <h3 className="project-box-title">Features</h3>
+                            <div className="my-3">
+                              {project.features.map((feature) => (
+                                <div className="d-flex align-items-start gap-2">
+                                  <img
+                                    src={checkCircle}
+                                    className="check-circle"
+                                    alt="check-circle"
+                                  />
+                                  <p className="project-des">{feature}</p>
+                                </div>
+                              ))}
+                            </div>
+                          </div>
+                          <div className="tech-used">
+                            <h3 className="project-box-title">Tech Used</h3>
+                            <div className="my-3 d-flex gap-3 flex-wrap">
+                              {project.technologies.map((tech) => (
+                                <div className="d-flex flex-row align-items-start gap-2">
+                                  <img
+                                    src={checkCircle}
+                                    className="check-circle"
+                                    alt="check-circle"
+                                  />
+                                  <p className="project-des">{tech}</p>
+                                </div>
+                              ))}
+                            </div>
+                          </div>
+                        </div>
+                      ) : (
+                        <div></div>
+                      )}
+                    </div>
                   </Card.Body>
                 </Card>
               </div>
